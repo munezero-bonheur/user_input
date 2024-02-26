eb8cccecb0699de879147b88fc74f09f433b8ee5
Munezero Bonheur <munezerobonheur@gmail.com>
2024-02-26 16:57:56 +0200

    User inputs using input function in  python.

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+
+age = input("Who old are you: ")
+
+location = input("Where are you located: ")
+
+print(f"your name is {name}, you are {age} years old and you are located in {location}.")
